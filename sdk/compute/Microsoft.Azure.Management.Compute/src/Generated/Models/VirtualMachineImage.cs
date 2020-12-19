713d69ac06fbe8737f3eabad9897915c7fa8b3e7
Pavan Rachapudy <vrachapu@microsoft.com>
2020-12-18 16:42:33 -0800

    Generated .Net SDK

--- sdk/compute/Microsoft.Azure.Management.Compute/src/Generated/Models/VirtualMachineImage.cs
+++ sdk/compute/Microsoft.Azure.Management.Compute/src/Generated/Models/VirtualMachineImage.cs
@@ -46,7 +46,7 @@ namespace Microsoft.Azure.Management.Compute.Models
         /// 'V2'</param>
         /// <param name="disallowed">Specifies disallowed configuration for the
         /// VirtualMachine created from the image</param>
-        public VirtualMachineImage(string name, string location, string id = default(string), IDictionary<string, string> tags = default(IDictionary<string, string>), PurchasePlan plan = default(PurchasePlan), OSDiskImage osDiskImage = default(OSDiskImage), IList<DataDiskImage> dataDiskImages = default(IList<DataDiskImage>), AutomaticOSUpgradeProperties automaticOSUpgradeProperties = default(AutomaticOSUpgradeProperties), string hyperVGeneration = default(string), DisallowedConfiguration disallowed = default(DisallowedConfiguration))
+        public VirtualMachineImage(string name, string location, string id = default(string), IDictionary<string, string> tags = default(IDictionary<string, string>), PurchasePlan plan = default(PurchasePlan), OSDiskImage osDiskImage = default(OSDiskImage), IList<DataDiskImage> dataDiskImages = default(IList<DataDiskImage>), AutomaticOSUpgradeProperties automaticOSUpgradeProperties = default(AutomaticOSUpgradeProperties), string hyperVGeneration = default(string), DisallowedConfiguration disallowed = default(DisallowedConfiguration), IList<VirtualMachineImageFeature> features = default(IList<VirtualMachineImageFeature>))
             : base(name, location, id, tags)
         {
             Plan = plan;
@@ -55,6 +55,7 @@ namespace Microsoft.Azure.Management.Compute.Models
             AutomaticOSUpgradeProperties = automaticOSUpgradeProperties;
             HyperVGeneration = hyperVGeneration;
             Disallowed = disallowed;
+            Features = features;
             CustomInit();
         }
 
@@ -95,6 +96,11 @@ namespace Microsoft.Azure.Management.Compute.Models
         /// </summary>
         [JsonProperty(PropertyName = "properties.disallowed")]
         public DisallowedConfiguration Disallowed { get; set; }
+
+        /// <summary>
+        /// </summary>
+        [JsonProperty(PropertyName = "properties.features")]
+        public IList<VirtualMachineImageFeature> Features { get; set; }
 
         /// <summary>
         /// Validate the object.
